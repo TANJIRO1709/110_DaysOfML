241a450de8c81504c0bb6b11acb620ea3e8185d7
Tanjiro_1709 <147371895+TANJIRO1709@users.noreply.github.com>
2024-07-02 22:30:13 +0530

    JSON AND SQL FILE HANDLING

---
+++ JSON_API.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+pd.read_json('train.json')
+
+
+# # LOADING JSON FROM AN URL
+
+# pd.read_json('url')
+
+# # WORKING WITH SQL DATA
+
+# In[3]:
+
+
+get_ipython().system('pip install mysql.connector')
+
+
+# In[5]:
+
+
+import mysql.connector
+
+
+# conn = mysql.connector.connect(host='localhost',user='postgres',password='',database='world')
+
+# pd.read_sql("SELECT * from city",conn )
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
