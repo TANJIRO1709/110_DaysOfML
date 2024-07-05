1db81e40134e92894db42ceda948bf8f0bfabb4c
Tanjiro_1709 <147371895+TANJIRO1709@users.noreply.github.com>
2024-07-05 12:10:41 +0530

    Working with API notes

---
+++ API.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import requests
+
+
+# requests.get('')
+
+# pd.DataFrame(response.json()['results']).head(2)[['vote','popularity'........]]
+
+# df = pd.DataFrame()
+
+# for i in range(1,429):
+#     response = requests.get(''.format(i))
+#     temp_df = pd.DataFrame(response.json()['results']).head(2)[['vote','popularity'........]]
+#     df.append(temp_df,ignore_index=True)
+
+# df.to_csv('movies.csv')
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
